0e9e37ce5f4baf05e8303a55751c9337faf0d288
dominoni.victor@gmail.com <dominoni.victor@gmail.com>
2019-08-29 17:59:31 -0300

    Adicionado Unit Test

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,19 @@
 from selenium import webdriver
+import unittest as ut
 
-#browserPath = "/usr/local/share/gecko_driver/"
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(ut.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+
+		self.assertIn('A-Fazer', self.browser.title)
+		self.fail('Acabar o teste!!')
+
+if __name__ == '__main__':
+	ut.main(warnings='ignore')
