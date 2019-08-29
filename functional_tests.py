94219d86a6567ae1bf1d3a8c3eda15304614c34f
dominoni.victor@gmail.com <dominoni.victor@gmail.com>
2019-08-29 19:08:53 -0300

    Teste funcional checa se colocamos um item na lista

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest as ut
+from time import sleep
 
 
 class NewVisitorTest(ut.TestCase):
@@ -12,7 +14,26 @@ class NewVisitorTest(ut.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 
-		self.assertIn('A-Fazer', self.browser.title)
+		title_text = self.browser.title
+		self.assertIn('A-Fazer', title_text)
+		
+		header_text = self.browser.find_element_by_tag_name('h1'.text)
+		self.assertIn('A-Fazer', header_text)
+
+		input_box = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+						input_box.get_attribute('placeholder'), 
+						'Digite seu A-Fazer')
+		input_box.send_keys('Comprar um desmafagafador')
+		input_box.send_keys(Keys.ENTER)
+		sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.browser.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Comprar um desmafagafador' for row in rows)
+			)
+
 		self.fail('Acabar o teste!!')
 
 if __name__ == '__main__':
